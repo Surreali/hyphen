7896089e5f9d5f3473e34492eeb1daa658d7552e
Surreali <123275062+Surreali@users.noreply.github.com>
2023-01-21 21:33:19 -0500

    Update hyphencode.js

--- hyphencode.js
+++ hyphencode.js
@@ -1,5 +1,5 @@
 $("#hyphenform").submit(function(e) {
-            if(document.getElementById("hyphencode").value == "less than three") {
+            if(document.getElementById("hyphencode").value == Math.round(Math.random()*Math.pow(10, 1)).toString()) {
                 e.preventDefault();
                 
                 document.getElementById("question").innerHTML = "Hyphen trial complete";
